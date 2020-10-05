7c71bd0e271a288929259ff576694137672d2570
craigh210 <craighunt210@gmail.com>
2020-10-05 15:03:14 -0400

    Initial commit

---
+++ src/test/java/edu/mc/jwd/SpringBoot505ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mc.jwd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot505ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
